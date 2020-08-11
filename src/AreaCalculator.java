de88af292fa688c689073b8e90bd09fca402dc12
Daniel Alejandro Lopez Munera <danielalejandrolopez97@gmail.com>
2020-08-11 12:25:04 +0200

    My solution for the exercise

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,2 +1,22 @@
 public class AreaCalculator {
+
+    public static double area(double radius){
+        if(radius >= 0){
+            double result = Math.PI * (radius * radius);
+            System.out.println("The area of the Cirle with radius: " + radius + ", is: " + result);
+            return result;
+        }else{
+            return -1;
+        }
+    }
+
+    public static double area(double firstSideRectangle, double secondSideRectangle){
+        if(firstSideRectangle >=0 && secondSideRectangle >= 0){
+            double result = firstSideRectangle * secondSideRectangle;
+            System.out.println("The area of the Rectangle with sides: " + firstSideRectangle + ", " + secondSideRectangle + " is: " + result);
+            return (result);
+        }else{
+            return -1;
+        }
+    }
 }
